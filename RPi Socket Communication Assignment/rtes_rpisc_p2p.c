262e82851587d87cc6dcee8130de81a7202aa1bc
Dimitris <47785842+dalxds@users.noreply.github.com>
2019-10-09 20:12:46 +0300

    Update rtes_rpisc_p2p.c
    chat server w/t libevent

--- RPi Socket Communication Assignment/rtes_rpisc_p2p.c
+++ RPi Socket Communication Assignment/rtes_rpisc_p2p.c
@@ -1,297 +1,256 @@
-#include <stdint.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <stdlib.h>
-#include <unistd.h>
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2011 Jason Ish
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+/*
+ * A simple chat server using libevent.
+ *
+ * @todo Check API usage with libevent2 proper API usage.
+ * @todo IPv6 support - using libevent socket helpers, if any.
+ */
+
 #include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <string.h>
-#include <time.h>
+
+/* Required by event.h. */
+#include <sys/time.h>
+
+#include <stdlib.h>
 #include <stdio.h>
-#include <getopt.h>
-#include <time.h>
-#include <assert.h>
-#include <stdbool.h>
-#include <pthread.h>
-#include <poll.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <err.h>
 
-//my files
-#include "rtes_rpisc_ringbuffer.h"
+/* On some systems (OpenBSD/NetBSD/FreeBSD) you could include
+ * <sys/queue.h>, but for portability we'll include the local copy. */
+#include <sys/queue.h>
 
-/**************************************************
-*                       INIT                      *
-***************************************************/
+/* Libevent. */
+#include <event2/event.h>
+#include <event2/event_struct.h>
+#include <event2/bufferevent.h>
+#include <event2/buffer.h>
 
-#define PORT_DEFAULT          2288
-#define AEM_LIST_LENGTH       50
-#define MAX_WAITING_CLIENTS   50
+/* Port to listen on. */
+#define SERVER_PORT 5555
 
-/* LIBEVENT FLAGS */
-#define EVLOOP_NO_EXIT_ON_EMPTY 0x04
+/* The libevent event base.  In libevent 1 you didn't need to worry
+ * about this for simple programs, but its used more in the libevent 2
+ * API. */
+static struct event_base *evbase;
 
-#define EV_TIMEOUT      0x01
-#define EV_READ         0x02
-#define EV_WRITE        0x04
-#define EV_SIGNAL       0x08
-#define EV_PERSIST      0x10
-#define EV_ET           0x20
+/**
+ * A struct for client specific data.
+ *
+ * This also includes the tailq entry item so this struct can become a
+ * member of a tailq - the linked list of all connected clients.
+ */
+struct client {
+  /* The clients socket. */
+  int fd;
 
-/**************************************************
-*                     STRUCTS                     *
-***************************************************/
+  /* The bufferedevent for this client. */
+  struct bufferevent *buf_ev;
 
-/* MAIN APP STRUCTS */
-typedef struct {
-  /* Client socket identifier */
-  int client_sock;
-  /* Address bound to the client socket interface
-   * this is NOT the peer address */
-  struct sockaddr_in local_addr;
-  /* Server socket identifier */
-  int server_sock;
-  /* Address bound to the server socket */
-  struct sockaddr_in server_addr;
-  /* */
-  void *user_data;
-} p2p_struct_t;
+  /*
+   * This holds the pointers to the next and previous entries in
+   * the tail queue.
+   */
+  STAILQ_ENTRY(client) entries;
+};  
 
-typedef struct {
-  //TODO: change to proper data types
-  uint32_t  id;
-  uint32_t  ip;
-  bool      status;
-  uint64_t  msgcounter;
-} p2p_node_t;
+/**
+ * The head of our tailq of all connected clients.  This is what will
+ * be iterated to send a received message to all connected clients.
+ */
+STAILQ_HEAD(, client) client_tailq_head;
 
-typedef struct {
-  uint32_t      aem_sender;
-  uint32_t      aem_receiver;
-  uint64_t      timestamp;
-  char          msg_body[256];
-} p2p_message_t;
+/**
+ * Set a socket to non-blocking mode.
+ */
+int
+setnonblock(int fd)
+{
+  int flags;
 
-/**************************************************
-*                    FUNCTIONS                    *
-***************************************************/
-
-/* MAIN APP FUNCTIONS */
-
-void p2p_init(p2p_struct_t *p2p) {
-  memset(p2p, 0, sizeof(p2p));
-}
-
-void p2p_close(p2p_struct_t *p2p) {
-  close(p2p->client_sock);
-  close(p2p->server_sock);
-}
-
-//p2p_listen(): p2p struct, host IP and port => server socket
-// -> the server part 1
-//socket(), bind(), listen()
-int p2p_listen(p2p_struct_t *p2p, char *host, char *port) {
-  int call_result; // returns the result of each call
-  int server_sock; // server socket descriptor
-  struct sockaddr_in server_addr;
-  int port_as_int;
-
-  port_as_int = atoi(port);
-  if(port_as_int <= 0) {
-    port_as_int = PORT_DEFAULT;
-  }
-
-  /* Create the socket, see p2p_join for an explanation of the parameters. */
-  if((server_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
-    return server_sock;
-  }
-
-  memset(&server_addr, 0, sizeof(server_addr));
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = htons(port_as_int);
-  server_addr.sin_addr.s_addr = inet_addr(host);
-  if(server_addr.sin_addr.s_addr == INADDR_NONE) {
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-  }
-
-  if((call_result = bind(server_sock, (struct sockaddr *) &server_addr, sizeof(server_addr))) != 0) {
-    close(server_sock);
-    return call_result;
-  }
-
-  if((call_result = listen(server_sock, MAX_WAITING_CLIENTS)) < 0) {
-    close(server_sock);
-    return call_result;
-  }
-
-  p2p->server_sock = server_sock;
-  p2p->server_addr = server_addr;
-  return server_sock;
-}
-
-//p2p_accept(): p2p struct => peer socket
-// -> the server part 2
-//accept()
-int p2p_accept(p2p_struct_t *p2p) {
-  int peer_socket;
-  struct sockaddr_in peer_addr;
-  socklen_t addr_ln;
-
-  /* accept a peer */
-  if((peer_socket = accept(p2p->server_sock, (struct sockaddr *) &peer_addr, &addr_ln)) < 0) {
-    close(peer_socket);
-  }
-
-  return peer_socket;
-}
-
-//p2p_join(): p2p struct, host IP and port => client socket
-// -> the client part
-//connect()
-int p2p_join(p2p_struct_t *p2p, char *host, char *port) {
-  struct sockaddr_in h_addr;
-  struct sockaddr_in l_addr;
-  int client_sock;
-  int call_state;
-  socklen_t l_add_len;
-
-  memset(&h_addr, 0, sizeof(h_addr));
-  /* We connect using TCP/Ipv4 */
-  h_addr.sin_family = AF_INET;
-  /* Parse the IP and format it for the low-level struct */
-  h_addr.sin_addr.s_addr = inet_addr(host);
-  /* Parse the port string in integer (atoi) and convert to network byte order
-   * (htons) */
-  h_addr.sin_port = htons(atoi(port));
-
-  if(h_addr.sin_addr.s_addr == INADDR_NONE) {
+  flags = fcntl(fd, F_GETFL);
+  if (flags < 0)
+    return flags;
+  flags |= O_NONBLOCK;
+  if (fcntl(fd, F_SETFL, flags) < 0)
     return -1;
-  }
 
-  /* Create the socket :
-   *  - AF_INET (equivalent to PF_INET) : IPv4
-   *  - SOCK_STREAM : a TCP STREAM
-   *  - IPPROTO_TCP : tells that the protocol is TCP (even if SOCK_STREAM
-   *    actualy implies it).
-   **/
-  if((client_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
-    /* Failed : return socket error code */
-    return client_sock;
-  }
-
-  if((call_state = connect(client_sock, (struct sockaddr *) &h_addr, sizeof(h_addr))) < 0) {
-    /* Failed : return connect error code */
-    close(client_sock);
-    return call_state;
-  }
-
-  /* get the IP of my bound interface */
-  if((call_state = getsockname(client_sock, (struct sockaddr *) &l_addr, &l_add_len)) < 0) {
-    close(client_sock);
-    return -1;
-  }
-
-  /** send msg
-  *  if(send(client_sock, &msg, sizeof(msg), 0) != sizeof(msg)) {
-  *   close(client_sock);
-  *   return -1;
-  * }
-  */
-
-  /* populate the p2p struct */
-  p2p->local_addr = l_addr;
-  p2p->client_sock = client_sock;
-  return client_sock;
+  return 0;
 }
 
-/* IO THREAD FUNCTIONS */
-void cb_func(evutil_socket_t fd, short what, void *arg) {
-  const char *data = arg;
-  printf("Got an event on socket %d:%s%s%s%s [%s]",
-         (int) fd,
-         (what & EV_TIMEOUT) ? " timeout" : "",
-         (what & EV_READ)    ? " read" : "",
-         (what & EV_WRITE)   ? " write" : "",
-         (what & EV_SIGNAL)  ? " signal" : "",
-         data);
-  if(what & EV_READ) {
-    //TODO: read from node and send to DATA WORKER
+/**
+ * Called by libevent when there is data to read.
+ */
+void
+buffered_on_read(struct bufferevent *bev, void *arg)
+{
+  struct client *this_client = arg;
+  struct client *client;
+  uint8_t data[8192];
+  size_t n;
+
+  /* Read 8k at a time and send it to all connected clients. */
+  for (;;) {
+    n = bufferevent_read(bev, data, sizeof(data));
+    if (n <= 0) {
+      /* Done. */
+      break;
+    }
+    
+    /* Send data to all connected clients except for the
+     * client that sent the data. */
+    STAILQ_FOREACH(client, &client_tailq_head, entries) {
+      if (client != this_client) {
+        bufferevent_write(client->buf_ev, data,  n);
+      }
+    }
   }
-  if(what & EV_WRITE){
-    //TODO: ask from DATA WORKER to send the next message for this node
-  }
-}
-
-/**************************************************
-*                THREAD FUNCTIONS                 *
-***************************************************/
-
-// IO THREAD
-// takes sockets
-// does the epoll wait
-// sends and receives data from data handler thread
-void *io_worker_main(void *args) {
-
-  //initialize event base
-  struct event_base *base = event_base_new();
-
-
-  // run event loop (event polling)
-  int event_base_loop(struct event_base * base, EVLOOP_NO_EXIT_ON_EMPTY);
-
-
 
 }
 
-// DATA HANDLER THREAD
-// controls the buffer
-// sends data to IO thread
-// receives data from IO thread and adds them to the buffer
-// produces the random message
-void *data_handler_main(void *args) {
-  //initialize buffer
-  circular_buf_t buffer;
-  cbuf_handle_t circular_buf_init(uint8_t *buffer, size_t size);
+/**
+ * Called by libevent when there is an error on the underlying socket
+ * descriptor.
+ */
+void
+buffered_on_error(struct bufferevent *bev, short what, void *arg)
+{
+  struct client *client = (struct client *)arg;
 
-  //TODO: tracks the items counter send for each node respectively (along with statust in AEM_list ?)
-
-}
-
-
-/**************************************************
-*                      MAIN                       *
-***************************************************/
-
-int main(int argc, char *argv[]) {
-  int listen = 0;
-  char *arg_ip;
-  char *arg_port;
-  p2p_struct_t p2p;
-
-  p2p_init(&p2p);
-
-  /* POLL SETUP */
-  struct pollfd fds[AEM_LIST_LENGTH];
-  memset(fds, 0, sizeof(fds));
-
-  /* IO WORKER */
-  pthread_t io_worker;
-
-  //create IO WORKER Thread
-  //TODO: encapsulate passing data (?)
-  if (pthread_create(&io_worker, NULL, io_worker_main, epfd) < 0) {
-    printf("An error occured: %d", err);
-    return -1;
+  if (what & BEV_EVENT_EOF) {
+    /* Client disconnected, remove the read event and the
+     * free the client structure. */
+    printf("Client disconnected.\n");
+  }
+  else {
+    warn("Client socket error, disconnecting.\n");
   }
 
-  //FOR SERVER AND CLIENT THREADS
-  //call function to create event
-  struct event *event_new(
-      struct event_base * base,
-      evutil_socket_t fd,
-      short what,
-      event_callback_fn cb,
-      void *arg);
-  //add event to event loop with no timeout
-  int event_add(struct event *ev, NULL);
+  /* Remove the client from the tailq. */
+  // STAILQ_REMOVE(&client_tailq_head, client, entries);
 
+  bufferevent_free(client->buf_ev);
+  close(client->fd);
+  free(client);
+}
 
+/**
+ * This function will be called by libevent when there is a connection
+ * ready to be accepted.
+ */
+void
+on_accept(int fd, short ev, void *arg)
+{
+  int client_fd;
+  struct sockaddr_in client_addr;
+  socklen_t client_len = sizeof(client_addr);
+  struct client *client;
+
+  client_fd = accept(fd, (struct sockaddr *)&client_addr, &client_len);
+  if (client_fd < 0) {
+    warn("accept failed");
+    return;
+  }
+
+  /* Set the client socket to non-blocking mode. */
+  if (setnonblock(client_fd) < 0)
+    warn("failed to set client socket non-blocking");
+
+  /* We've accepted a new client, create a client object. */
+  client = calloc(1, sizeof(*client));
+  if (client == NULL)
+    err(1, "malloc failed");
+  client->fd = client_fd;
+
+  client->buf_ev = bufferevent_socket_new(evbase, client_fd, 0);
+  bufferevent_setcb(client->buf_ev, buffered_on_read, NULL,
+      buffered_on_error, client);
+
+  /* We have to enable it before our callbacks will be
+   * called. */
+  bufferevent_enable(client->buf_ev, EV_READ);
+
+  /* Add the new client to the tailq. */
+  STAILQ_INSERT_TAIL(&client_tailq_head, client, entries);
+
+  printf("Accepted connection from %s\n", 
+      inet_ntoa(client_addr.sin_addr));
+}
+
+int
+main(int argc, char **argv)
+{
+  int listen_fd;
+  struct sockaddr_in listen_addr;
+  struct event ev_accept;
+  int reuseaddr_on;
+
+  /* Initialize libevent. */
+        evbase = event_base_new();
+
+  /* Initialize the tailq. */
+  STAILQ_INIT(&client_tailq_head);
+
+  /* Create our listening socket. */
+  listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (listen_fd < 0)
+    err(1, "listen failed");
+  memset(&listen_addr, 0, sizeof(listen_addr));
+  listen_addr.sin_family = AF_INET;
+  listen_addr.sin_addr.s_addr = INADDR_ANY;
+  listen_addr.sin_port = htons(SERVER_PORT);
+  if (bind(listen_fd, (struct sockaddr *)&listen_addr,
+    sizeof(listen_addr)) < 0)
+    err(1, "bind failed");
+  if (listen(listen_fd, 5) < 0)
+    err(1, "listen failed");
+  reuseaddr_on = 1;
+  setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &reuseaddr_on, 
+      sizeof(reuseaddr_on));
+
+  /* Set the socket to non-blocking, this is essential in event
+   * based programming with libevent. */
+  if (setnonblock(listen_fd) < 0)
+    err(1, "failed to set server socket to non-blocking");
+
+  /* We now have a listening socket, we create a read event to
+   * be notified when a client connects. */
+        event_assign(&ev_accept, evbase, listen_fd, EV_READ|EV_PERSIST, 
+      on_accept, NULL);
+  event_add(&ev_accept, NULL);
+
+  /* Start the event loop. */
+  event_base_dispatch(evbase);
+
+  return 0;
 }
