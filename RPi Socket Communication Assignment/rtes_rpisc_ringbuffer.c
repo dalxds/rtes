04fc3ea67e239340e7dd662bc7070e2288a8b52c
Dimitris <47785842+dalxds@users.noreply.github.com>
2019-12-05 01:13:06 +0200

    Added Ring Buffer

--- RPi Socket Communication Assignment/rtes_rpisc_ringbuffer.c
+++ RPi Socket Communication Assignment/rtes_rpisc_ringbuffer.c
@@ -1,108 +1,180 @@
-#include "rtes_rpisc_ringbuffer.h"
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <assert.h>
 
-// The hidden definition of our circular buffer structure
-struct circular_buf_t {
-	uint8_t *buffer;
+/// Opaque circular buffer structure
+typedef struct circular_buffer circular_buffer;
+
+/// Handle type, the way users interact with the API
+typedef circular_buffer *cbuf;
+
+/// Pass in a storage buffer and size, returns a circular buffer handle
+/// Requires: buffer is not NULL, size > 0
+/// Ensures: buffer has been created and is returned in an empty state
+cbuf circular_buf_init(uint8_t *buffer, size_t size);
+
+/// Put version 1 continues to add data if the buffer is over_max
+/// Old data is overwritten
+/// Requires: buffer is valid and created by circular_buf_init
+void circular_buf_add(cbuf buffer, uint8_t data);
+
+/// Read a value from the buffer
+/// Requires: buffer is valid and created by circular_buf_init
+/// Returns 0 on success, -1 if the buffer is empty
+int circular_buf_read(cbuf buffer, uint8_t *data, size_t index);
+
+/// Free a circular buffer structure
+/// Requires: buffer is valid and created by circular_buf_init
+/// Does not free data buffer; owner is responsible for that
+void circular_buf_free(cbuf buffer);
+
+/// Reset the circular buffer to empty, head == tail. Data not cleared
+/// Requires: buffer is valid and created by circular_buf_init
+void circular_buf_reset(cbuf buffer);
+
+/// Check the current value of the index
+/// Requires: buffer is valid and created by circular_buf_init
+/// Returns the current number of the index (all messages written to buffer)
+size_t circular_buf_size(cbuf buffer);
+
+/// Checks if the buffer is empty
+/// Requires: buffer is valid and created by circular_buf_init
+/// Returns true if the buffer is empty
+bool circular_buf_empty(cbuf buffer);
+
+/// Checks if the buffer is over_max
+/// Requires: buffer is valid and created by circular_buf_init
+/// Returns true if the buffer is over_max
+bool circular_buf_over_max(cbuf buffer);
+
+// The definition of our circular buffer structure is hidden from the user
+// NOTE: write on the report about index being size_t
+// NOTE: index and head show on the next item (that's going to be added)
+struct circular_buffer {
+	uint8_t *buffer_obj;
 	size_t head;
-	size_t tail;
+	size_t index; //id number to keep track of messages when buf size > max
 	size_t max; //of the buffer
-	bool full;
-	size_t message_id; //overall message_id count
+	bool over_max;
 };
 
-// Return a struct
-cbuf_handle_t circular_buf_init(uint8_t *buffer, size_t size) {
-	assert(buffer && size);
-	cbuf_handle_t cbuf = malloc(sizeof(circular_buf_t));
-	assert(cbuf);
-	cbuf->buffer = buffer;
-	cbuf->max = size;
-	circular_buf_reset(cbuf);
-	assert(circular_buf_empty(cbuf));
-	return cbuf;
+#define EXAMPLE_BUFFER_SIZE 10
+
+cbuf circular_buf_init(uint8_t *buffer_obj, size_t size) {
+	assert(buffer_obj && size);
+	cbuf buffer = malloc(sizeof(circular_buffer));
+	assert(buffer);
+	buffer->buffer_obj = buffer_obj;
+	buffer->max = size;
+	circular_buf_reset(buffer);
+	assert(circular_buf_empty(buffer));
+	return buffer;
 }
 
-static void advance_pointer(cbuf_handle_t cbuf) {
-	assert(cbuf);
+void circular_buf_add(cbuf buffer, uint8_t data) {
+	assert(buffer && buffer->buffer_obj);
 
-	if(cbuf->full) {
-		cbuf->tail = (cbuf->tail + 1) % cbuf->max;
+	buffer->head = (buffer->head + 1) % buffer->max;
+	buffer->index++;
+
+	buffer->buffer_obj[buffer->head] = data;
+
+	if(!buffer->over_max) buffer->over_max = (buffer->head == 0 && buffer->index > 0);
+}
+
+int circular_buf_read(cbuf buffer, uint8_t *data, size_t index) {
+	assert(buffer && data && buffer->buffer_obj);
+
+	if(circular_buf_empty(buffer)) return -1;
+
+	if(index > buffer->index) return -2;
+
+	if(buffer->over_max) {
+		if (index <= buffer->index - EXAMPLE_BUFFER_SIZE) return -3;
+
+		*data = buffer->buffer_obj[index % buffer->max];
+		return 0;
 	}
 
-	cbuf->head = (cbuf->head + 1) % cbuf->max;
-	cbuf->full = (cbuf->head == cbuf->tail);
+	*data = buffer->buffer_obj[index];
+	return 0;
 }
 
-static void retreat_pointer(cbuf_handle_t cbuf) {
-	assert(cbuf);
-	cbuf->full = false;
-	cbuf->tail = (cbuf->tail + 1) % cbuf->max;
+void circular_buf_free(cbuf buffer) {
+	assert(buffer);
+	free(buffer);
 }
 
-void circular_buf_reset(cbuf_handle_t cbuf) {
-	assert(cbuf);
-	cbuf->head = 0;
-	cbuf->tail = 0;
-	cbuf->full = false;
+void circular_buf_reset(cbuf buffer) {
+	assert(buffer);
+	buffer->head = 0;
+	buffer->index = 0;
+	buffer->over_max = false;
 }
 
-void circular_buf_free(cbuf_handle_t cbuf) {
-	assert(cbuf);
-	free(cbuf);
+size_t circular_buf_size(cbuf buffer) {
+	assert(buffer);
+	return buffer->index + 1;
 }
 
-bool circular_buf_full(cbuf_handle_t cbuf) {
-	assert(cbuf);
-	return cbuf->full;
+bool circular_buf_empty(cbuf buffer) {
+	assert(buffer);
+	return (!buffer->over_max && (buffer->head == 0));
 }
 
-bool circular_buf_empty(cbuf_handle_t cbuf) {
-	assert(cbuf);
-	return (!cbuf->full && (cbuf->head == cbuf->tail));
+
+void print_buffer_status(cbuf buffer) {
+	printf("\nSTATUS === Over max: %d, empty: %d, index: %zu, head: %zu ===\n",
+				 buffer->over_max,
+				 circular_buf_empty(buffer),
+				 buffer->index,
+				 buffer->head);
 }
 
-size_t circular_buf_capacity(cbuf_handle_t cbuf) {
-	assert(cbuf);
-	return cbuf->max;
-}
+int main(int argc, char const *argv[]) {
+	uint8_t *buffer_obj  = malloc(EXAMPLE_BUFFER_SIZE * sizeof(uint8_t));
+	printf("\n=== C Circular Buffer Check ===\n");
+	cbuf buffer = circular_buf_init(buffer_obj, EXAMPLE_BUFFER_SIZE);
+	printf("Buffer initialized.\n");
+	print_buffer_status(buffer);
 
-size_t circular_buf_size(cbuf_handle_t cbuf) {
-	assert(cbuf);
-	size_t size = cbuf->max;
+	printf("\n******Filling buffer******\n");
 
-	if(!cbuf->full) {
-		if(cbuf->head >= cbuf->tail) {
-			size = (cbuf->head - cbuf->tail);
-
-		} else {
-			size = (cbuf->max + cbuf->head - cbuf->tail);
-		}
+	for(uint8_t i = 0; i < (EXAMPLE_BUFFER_SIZE - 1); i++) {
+		circular_buf_add(buffer, i * 5);
+		printf("value %u *** index %zu *** head %zu \n",
+					 i * 5,
+					 buffer->index,
+					 buffer->head);
 	}
 
-	return size;
-}
+	printf("\n******Printing buffer******\n");
 
-size_t circular_buf_count(cbuf_handle_t cbuf) {
-	assert(cbuf);
-	return cbuf->message_id;
-}
-
-void circular_buf_put(cbuf_handle_t cbuf, uint8_t data) {
-	assert(cbuf && cbuf->buffer);
-	cbuf->buffer[cbuf->head] = data;
-	cbuf->message_id++;
-	advance_pointer(cbuf);
-}
-
-int circular_buf_get(cbuf_handle_t cbuf, uint8_t *data_recv) {
-	assert(cbuf && data_recv && cbuf->buffer);
-	int r = -1;
-
-	if(!circular_buf_empty(cbuf)) {
-		*data_recv = cbuf->buffer[cbuf->tail];
-		retreat_pointer(cbuf);
-		r = 0;
+	for(uint8_t i = 0; i < (EXAMPLE_BUFFER_SIZE); i++) {
+		printf("value %i *** pseudoindex %i\n",
+					 buffer->buffer_obj[i],
+					 i);
 	}
 
-	return r;
+	print_buffer_status(buffer);
+	printf("\n******Reading values******\n");
+	int input;
+
+	while(1) {
+		printf("\nEnter Index:\n");
+		if (scanf("%i", &input) != 1)
+            break;
+		uint8_t data;
+		int data_return = circular_buf_read(buffer, &data, input);
+		printf("\nRead Function Status --> %i <--\n", data_return );
+
+		if(!data_return)
+			printf("Data: %u\n", data);
+	}
+
+	printf("\n******EXIT******\n");
+	return 0;
 }
